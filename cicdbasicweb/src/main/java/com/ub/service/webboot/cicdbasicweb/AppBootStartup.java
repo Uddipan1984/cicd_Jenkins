a550497dc4dd5dc8486844fcab6a2e771132fde9
UDDIPAN BASU <44173693+Uddipan1984@users.noreply.github.com>
2019-10-11 00:04:19 -0400

    Update AppBootStartup.java
    //Comment added

--- cicdbasicweb/src/main/java/com/ub/service/webboot/cicdbasicweb/AppBootStartup.java
+++ cicdbasicweb/src/main/java/com/ub/service/webboot/cicdbasicweb/AppBootStartup.java
@@ -4,7 +4,7 @@ public class AppBootStartup {
 
 	public static void main(String[] args) {
 		System.out.println("Application started.");
-		
+		//Here we start spring web with dispatcher servlet using Spring Boot
 		
 		System.out.println("end of the application.");
 
