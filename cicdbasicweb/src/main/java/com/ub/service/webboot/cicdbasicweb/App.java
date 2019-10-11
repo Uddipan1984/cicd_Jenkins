24891488501e44427f57604b55388b7344d5b187
uddipanbasu <uddipanbasu@10.0.0.137>
2019-10-10 23:53:56 -0400

    First Check in for Jenkins CI/CD proof of concept

---
+++ cicdbasicweb/src/main/java/com/ub/service/webboot/cicdbasicweb/App.java
@@ -0,0 +1,13 @@
+package com.ub.service.webboot.cicdbasicweb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
