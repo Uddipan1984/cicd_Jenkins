bc82246502684df2cfa4d4444fc890f70bfe8391
uddipanbasu <uddipanbasu@10.0.0.137>
2019-10-11 00:02:06 -0400

    First class commit

---
+++ cicdbasicweb/src/main/java/com/ub/service/webboot/cicdbasicweb/AppBootStartup.java
@@ -0,0 +1,13 @@
+package com.ub.service.webboot.cicdbasicweb;
+
+public class AppBootStartup {
+
+	public static void main(String[] args) {
+		System.out.println("Application started.");
+		
+		
+		System.out.println("end of the application.");
+
+	}
+
+}
