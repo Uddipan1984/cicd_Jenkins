0637c0dd993e6cd660dc87f9097260a9e2c23b79
uddipanbasu <uddipanbasu@10.0.0.137>
2019-12-10 01:02:47 -0500

    Host rest code changes

--- cicdbasicweb/src/main/java/com/ub/service/webboot/cicdbasicweb/controller/HomeRestController.java
+++ cicdbasicweb/src/main/java/com/ub/service/webboot/cicdbasicweb/controller/HomeRestController.java
@@ -1,5 +1,7 @@
 package com.ub.service.webboot.cicdbasicweb.controller;
 
+import java.net.InetAddress;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,23 @@ public class HomeRestController {
 	
 	@RequestMapping("/")
 	public String homecontext() {
-	return "Its working now. Small Rest service , Stepped in Home Context";
+	return "<h2>Its working now. Small Rest service , Stepped in Home Context</h2></br>"+ getHostDetails();
+	}
+	
+	public static String getHostDetails () {
+		
+		InetAddress iAdress = null;
+		String hostName     = null;
+		
+		try {
+			iAdress = InetAddress.getLocalHost();
+			hostName= iAdress.getHostName()  + " - " + iAdress.getHostAddress()   ;
+		}catch (Throwable err) {
+			hostName = "No Host";
+			System.err.println("Error - "+ err.toString());
+		}
+		
+		return hostName;
 	}
 
 }
